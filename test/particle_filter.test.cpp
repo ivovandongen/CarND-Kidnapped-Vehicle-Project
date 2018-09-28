e080fd2206d2ee5397ceff4d54c4a3e75bcd374c
Ivo van Dongen <info@ivovandongen.nl>
2018-09-28 15:33:04 +0300

    particle filter init

--- test/particle_filter.test.cpp
+++ test/particle_filter.test.cpp
@@ -2,9 +2,14 @@
 
 #include <particle_filter.h>
 
+#include <cmath>
 #include <vector>
 
-TEST(ParticleFilter, BasicTest) {
-    ParticleFilter filter;
-    ASSERT_TRUE(true);
+TEST(ParticleFilter, Init) {
+    ParticleFilter filter {10};
+
+    double std_dev[] = {.3, .3, .01};
+    filter.init(10, 10, M_PI_2 / 2, std_dev);
+
+    ASSERT_EQ(10, filter.particles.size());
 }
